f0725d587e83ee6d63c51bc1dc5b5b6f1a730992
devapptechnologies <support@apptechnologies.co>
2025-12-15 10:30:21 +0545

    loading-screen

--- components/loading-screen.tsx
+++ components/loading-screen.tsx
@@ -15,7 +15,7 @@ export function LoadingScreen() {
 
       <div className="relative z-10 flex flex-col items-center justify-center">
         {/* Logo Container */}
-        <div className="relative w-80  mb-6">
+        <div className="relative w-80 mb-6">
           <div className="relative w-40 h-10 mx-auto">
             <Image
               src="/logo/dark-theme-logo.png"
